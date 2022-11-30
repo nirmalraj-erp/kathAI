3fcec802e405d8da24a84ff2621d0b3376b032a1
NIRMALRAJ RETHINASABAPATHI <64870594+nirmalraj-erp@users.noreply.github.com>
2022-11-30 19:35:41 +0530

    initial commit
    initial commit

---
+++ kathAI_app/model/run.py
@@ -0,0 +1,24 @@
+from whisper import whisper
+
+model = whisper.load_model("base")
+
+# load audio and pad/trim it to fit 30 seconds
+audio = whisper.load_audio("/kathAI_app/data/audio.mp3")
+print(f"Audio ################## \n{audio}")
+audio = whisper.pad_or_trim(audio)
+print(f"Audio ################## \n{audio}")
+
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
+print(f"mel #################### \n{mel}")
+
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
+
+# decode the audio
+options = whisper.DecodingOptions(fp16=False)
+result = whisper.decode(model, mel, options)
+
+# print the recognized text
+print(result.text)
